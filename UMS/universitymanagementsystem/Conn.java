c48990ae693bc249f39f89f1efb70ab5965bf1d2
torikulislampatoary399 <torikulislampatoary399@gmail.com>
2026-05-13 10:43:59 -0700

    Add files via upload

---
+++ UMS/universitymanagementsystem/Conn.java
@@ -0,0 +1,20 @@
+package universitymanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Conn {
+
+    Connection c;
+    Statement s;
+    Conn(){
+          try {
+              Class.forName("com.mysql.cj.jdbc.Driver");
+              c=DriverManager.getConnection("jdbc:mysql://localhost:3306/university","root","12Qw34er@#");
+              s = c.createStatement();
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+    }
+}
